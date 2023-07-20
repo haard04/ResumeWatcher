8d7f3c89b50e26eb4bf072b926bfc5dcb4bcdd60
haard04 <haardshah04@gmail.com>
2023-07-20 21:41:41 +0530

    static pdf are opening

--- resumewatcher/settings.py
+++ resumewatcher/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+# print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -120,9 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pdfs'),  # Add the PDF files folder to static files dirs
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
