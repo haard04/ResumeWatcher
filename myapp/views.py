55f3e029c81342c8c59f61a923ee29dd10ea576e
haard04 <haardshah04@gmail.com>
2023-10-13 10:26:26 +0530

    nlp added

--- myapp/views.py
+++ myapp/views.py
@@ -10,7 +10,9 @@ import PyPDF2
 import re
 import json
 import io
-
+import nltk
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 
@@ -87,6 +89,7 @@ def upload_pdf(request):
 from django.http import HttpResponse
 from .models import MyModel
 
+@api_view(['GET'])
 def view_pdf(request, username):
     try:
         my_model_instance = MyModel.objects.get(username=username)
@@ -104,7 +107,7 @@ def view_pdf(request, username):
         return HttpResponse('PDF not found', status=404)
 
 
-
+@api_view(['POST'])
 def save_pdf(request, filename):
     if request.method == 'POST':
         pdf_file = request.FILES.get('pdf_file')
@@ -144,7 +147,7 @@ def jobform(request):
     user_id = request.user.id
     return render(request,'createjob.html',{'user_id': user_id})
 
-
+@api_view(['POST'])
 def add_job_to_profile(request, user_id):
     user = get_object_or_404(MyModel, pk=user_id)
 
@@ -184,6 +187,7 @@ def add_job_to_profile(request, user_id):
 
     return JsonResponse({'message': 'Invalid request method.'})
 
+@api_view(['GET'])
 def get_all_jobs(request, user_id):
     # Get the MyModel instance based on user ID
     user_instance = get_object_or_404(MyModel, pk=user_id)
@@ -255,6 +259,7 @@ def delete_job(request, job_id):
     job.delete()
     return JsonResponse({'message': 'Job successfully deleted.'})
 
+@api_view(['GET'])
 def get_job_by_id(request, job_id):
     job = get_object_or_404(Job, pk=job_id)
     job_data = {
@@ -271,10 +276,79 @@ def get_job_by_id(request, job_id):
     }
     return JsonResponse({'job': job_data})
 
+@api_view(['GET'])
 def matchskill(request):
+    jobdesc = """
+    Job Summary
+    About Rubrik
+
+    Rubrik is one of the fastest-growing companies in Silicon Valley, revolutionizing data protection, and management in the emerging multi-cloud world. We are the leader in cloud data management, delivering a single platform to manage and protect data in the cloud, at the edge, and on-premises. Enterprises choose Rubrik to simplify backup and recovery, accelerate cloud adoption, enable automation at scale, and secure against cyberthreats. We’ve been recognized as a Forbes Cloud 100 Company two years in a row and as a LinkedIn Top 10 startup in 2018. 
+
+    Responsibilities
+
+    Design, develop, test, deploy, maintain and improve the software.
+    Manage individual projects priorities, deadlines, and deliverables with your technical expertise.
+    Identify and solve bottlenecks within our software stack.
+    Qualifications
+
+    Rubrik Software Engineer - Interns are self-starters, driven, and can manage themselves.  Bottom line, if you have a limitless drive and like to win, we want to talk to you - come make history! 
+
+    Excellent analytical and mathematics skills
+    Ability to work in teams
+    Experience programming in Python, Java, Python, C# or C/C++
+    Strong understanding of data structures, algorithms, and object-oriented programming
+    Strong communication skills
+    Eligibility Criteria: 
+
+    Strong programming language skills, and impressive interpersonal abilities so that they can collaborate effectively with other members of their team.
+    Applicants must be currently pursuing a Bachelor’s degree or higher in Computer Science / IT, EC, Mathematics and Computing major.
+    CGPA 8 and above
+    The Internship is scheduled between Jan 2024 - June 2024, and you will be expected to be in our Bangalore office for the entire duration.
+    Benefits
+
+    Competitive Stipend
+    Flight tickets (within India) booking is provided to travel to Bangalore.
+    Breakfast, lunch, dinner, and snacks on a daily basis
+    Cab facility within Bangalore
+    About Rubrik:
+
+    Rubrik, the Zero Trust Data Security Company™, delivers data security and operational resilience for enterprises. Rubrik’s big idea is to provide data security and data protection on a single platform, including Zero Trust Data Protection, Ransomware Investigation, Incident Containment, Sensitive Data Discovery, and Orchestrated Application Recovery. This means your data is ready so you can recover the data you need, and avoid paying a ransom. Because when you secure your data, you secure your applications, and you secure your business.
+
+    We are a leader in data security, have been recognized as a Forbes Cloud 100 Company, named as a LinkedIn Top 10 Startup and are proud to have earned Great Place to Work® Certification™. There has never been a more exciting time to join Rubrik, and our future is even brighter. The work you do will help propel our next chapter of growth as you do the best work of your career.
+
+    Linkedin | Twitter | Instagram | Rubrik.com | 
+
+    Diversity, Equity & Inclusion @ Rubrik: 
+
+    At Rubrik we are committed to building and sustaining a culture where people of all backgrounds are valued, know they belong, and believe they can succeed here.
+
+    Rubrik's goal is to hire and promote the best person for the job, no matter their background. In doing so, Rubrik is committed to correcting systemic processes and cultural norms that have prevented equal representation. This means we review our current efforts with the intent to offer fair hiring, promotion, and compensation opportunities to people from historically underrepresented communities, and strive to create a company culture where all employees feel they can bring their authentic selves to work and be successful.
+
+    Our DEI strategy focuses on three core areas of our business and culture:
+
+    Our Company: Build a diverse company that provides equitable access to growth and success for all employees globally. 
+
+    Our Culture: Create an inclusive environment where authenticity thrives and people of all backgrounds feel like they belong.
+
+    Our Communities: Expand our commitment to diversity, equity, & inclusion within and beyond our company walls to invest in future generations of underrepresented talent and bring innovation to our clients.
+
+    Equal Opportunity Employer/Veterans/Disabled: Rubrik is an Equal Opportunity Employer. All qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, or protected veteran status and will not be discriminated against on the basis of disability.
+
+    Rubrik provides equal employment opportunities (EEO) to all employees and applicants for employment without regard to race, color, religion, sex, national origin, age, disability or genetics. In addition to federal law requirements, Rubrik complies with applicable state and local laws governing nondiscrimination in employment in every location in which the company has facilities. This policy applies to all terms and conditions of employment, including recruiting, hiring, placement, promotion, termination, layoff, recall, transfer, leaves of absence, compensation and training. 
+
+    Federal law requires employers to provide reasonable accommodation to qualified individuals with disabilities. Please contact us at hr@rubrik.com if you require a reasonable accommodation to apply for a job or to perform your job. Examples of reasonable accommodation include making a change to the application process or work procedures, providing documents in an alternate format, using a sign language interpreter, or using specialized equipment.
+
+    EEO IS THE LAW
+
+    EEO IS THE LAW - POSTER SUPPLEMENT
+
+    PAY TRANSPARENCY NONDISCRIMINATION PROVISION
+
+    NOTIFICATION OF EMPLOYEE RIGHTS UNDER FEDERAL LABOR LAWS
+    """
     Skills = ["Python", "Java", "R", "JavaScript", "SQL", "HTML", "CSS", "Machine Learning", "TensorFlow", "Pandas", "Seaborn"]
-    Required_skills = ["Python", "R", "TensorFlow"]
-    matched_words = {'skills': []}
+    Required_skills = getskillsfromdesc(jobdesc)
+    matched_words = {'skills': []} 
 
     def search_pdf_for_words(pdf_file, words):
 
@@ -306,12 +380,34 @@ def matchskill(request):
        
         matching_skills = list(set(matched_words['skills']) & set(Required_skills))
         percentage_matched = (len(matching_skills) / len(Required_skills)) * 100
-        
-
+          
+        print(f"Required_skills are:{Required_skills}")
         print("Skills in Resume: ", matched_words)
         print("Skills that match with required skills:", matching_skills)
         print(f"{percentage_matched:.2f}% of required skills are matched")
     else:
         print("PDF not found for the user.")
     return render(request,'hello.html') # matched_words,matching_skills,percentage_matched
+
+
+
+def getskillsfromdesc(jobdesc):
+    # Define a list of common skills
+    common_skills = ["Python", "Java", "R", "JavaScript", "SQL", "HTML", "CSS", "Machine Learning", "TensorFlow", "Pandas", "Seaborn"]
+
+    # Extract skills from the job description
+    extracted_skills = []
+
+    # Tokenize the text
+    words = nltk.word_tokenize(jobdesc)
+
+    # Check for skills in the text
+    for skill in common_skills:
+        # Use regular expression to match skill names
+        pattern = re.compile(re.escape(skill), re.IGNORECASE)
+        if any(re.search(pattern, word) for word in words):
+            extracted_skills.append(skill)
+
+    # Print the extracted skills
+    return extracted_skills
 
